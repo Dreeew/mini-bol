b1ab504f7f74f25c77c3966c24db15c0bd8da018
Drew <developed.by.drew@gmail.com>
2017-11-03 16:23:00 +0000

    Adds lightbox and img/title tags to it

--- js/box-of-light.js
+++ js/box-of-light.js
@@ -1,14 +1,31 @@
 //Gets container
 let bol_container = $(".bol-container");
+let bol_overlay = $("#bol-overlay");
 
-bol_container.find("ul li").click(function(){
-	$("#bol-overlay").css({"visibility":"visible"});
+//Detects click on an image item and activates the overlay
+bol_container.find("ul li img").click(function(){
+	bol_overlay.css({"visibility":"visible"});
+
+	//Creates lightbox and assigns it to a variable
+	bol_container.prepend('<div id="bol-lightbox"></div>');
+	bol_lightbox = $("#bol-lightbox");
+
+	bol_lightbox.append('<div id="bol-lightbox-title">Sample Text Here</div>')
+	bol_lightbox.append('<img src="img/1.jpg" alt="test">');
+
+
+
+});
+
+//Deactivates the overlay
+bol_overlay.click(function(){
+	$(this).css({"visibility":"hidden"});
+	$("#bol-lightbox").remove();
+});
+
+$("#bol-lightbox").click(function(){
+	preventDefault();
 })
-
-$("#bol-overlay").click(function(){
-	$(this).css({"visibility":"hidden"})
-})
-
 
 /*** Create Array of Images***/
 //Finds all img tags
